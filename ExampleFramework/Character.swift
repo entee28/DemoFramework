45a0808d8418afeb5b073ab33240bc3f8ac42022
Thanh Nguyen <puffle.nnt@gmail.com>
2024-06-13 14:26:38 +0700

    first commit

---
+++ ExampleFramework/Character.swift
@@ -0,0 +1,16 @@
+//
+//  Character.swift
+//  ExampleFramework
+//
+//  Created by Nguyễn Nhật Thành on 13/6/24.
+//
+
+import Foundation
+
+public class Character {
+    public let name: String
+    
+    public init(name: String) {
+        self.name = name
+    }
+}
---
+++ ExampleFramework/ExampleFramework.h
@@ -0,0 +1,18 @@
+//
+//  ExampleFramework.h
+//  ExampleFramework
+//
+//  Created by Nguyễn Nhật Thành on 13/6/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ExampleFramework.
+FOUNDATION_EXPORT double ExampleFrameworkVersionNumber;
+
+//! Project version string for ExampleFramework.
+FOUNDATION_EXPORT const unsigned char ExampleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExampleFramework/PublicHeader.h>
+
+
